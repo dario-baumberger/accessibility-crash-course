34645c15ba0865780ff3ebf14aa6f94fa55ce053
Dario Baumberger <dario.baumberger@gmail.com>
2020-11-06 22:53:08 +0100

    content

--- rename.sh
+++ rename.sh
@@ -1,9 +1,12 @@
 for file in $(ls content/slides/); do
   f=$(echo "${file##*/}");
    filename=$(echo $f| cut  -d'.' -f 1); #file has extension, it return only filename
-		new=$(($filename + 1))
-		echo $filename $new
-		mv "content/slides/$file" "content/temp_slides/${new}.md" ;
+   if [ $filename > 22 ]
+      then
+      new=$(($filename + 1))
+      echo $filename $new
+      mv "content/slides/$file" "content/temp_slides/${new}.md" ;
+    fi
 done
 
 for file in $(ls content/temp_slides/); do
